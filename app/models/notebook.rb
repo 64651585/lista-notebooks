4bd8af73cd765d8451d38b9d90955008950f0dc5
64651585 <eduardoksa@protonmail.ch>
2022-03-19 01:59:51 -0300

    alterações no front-end e validacoes de campos através no model

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,2 +1,9 @@
 class Notebook < ApplicationRecord
+
+    validates :marca, presence: true
+    validates :preco, presence: true
+    validates :sistema_operacional, presence: true
+    validates :armanezamento, presence: true
+    validates :memoria, presence: true
+    validates :processador, presence: true
 end
